f7b8e7eb1856d17516461b3dd5966bbfccd5fa75
m.morasaeiyan <m.morasaeiyan@irantalent.com>
2025-01-01 11:27:15 +0330

    init project for devlop

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import "@/app/ui/global.css"
+import { inter } from "@/app/ui/fonts";
+import clsx from "clsx";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={clsx(inter.className,'antialiased')}>{children}</body>
+    </html>
+  );
+}
